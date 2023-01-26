94c28312ff1651cbadd93d245bafea1f4fe4d947
serue <serudzaicollen@gmail.com>
2023-01-26 09:51:55 +0200

    create method in serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -6,10 +6,22 @@ class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True) # renaming a property object from the product model
     url = serializers.SerializerMethodField(read_only=True) 
     preferred_url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
+
+    email =  serializers.EmailField(write_only=True)
+
     class Meta:
         model =Product
-        fields=['preferred_url','url','pk','title', 'content','price', 'sale_price','discount'] 
+        fields=['preferred_url','url','email','pk','title', 'content','price', 'sale_price','discount'] 
     
+
+    # def create(self, validated_data):
+    #     #return Product.objects.create(**validated_data)
+    #     email = validated_data.pop('email')
+    #     return super().create(validated_data)
+
+    # def update(self, instance, validated_data):
+    #     instance.title=validated_data.get('title')
+    #     return instance
 
     def get_url(self, obj):
         request = self.context.get('request')
