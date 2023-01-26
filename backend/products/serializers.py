d39a6bc0233d151edbea63869c6eaea5b44edaf8
serue <serudzaicollen@gmail.com>
2023-01-26 09:28:53 +0200

    urls reverse in serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
-
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True) # renaming a property object from the product model
+    url = serializers.SerializerMethodField(read_only=True) 
+    preferred_url = serializers.HyperlinkedIdentityField(view_name='product-detail', lookup_field='pk')
     class Meta:
         model =Product
-        fields=['pk','title', 'content','price', 'sale_price','discount'] 
+        fields=['preferred_url','url','pk','title', 'content','price', 'sale_price','discount'] 
     
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+
     def get_discount(self, obj):
         if not hasattr(obj, 'id'):
             return None
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('new/', views.ProductCreateAPIView.as_view()),
-    path('list/', views.ProductListAPIView.as_view()),
-    path('<str:pk>/', views.ProductDetailAPIView.as_view()),
-     path('<str:pk>/update/', views.ProductUpdateAPIView.as_view()),
-      path('<str:pk>/destroy/', views.ProductDestroyAPIView.as_view()),
+    path('new/', views.ProductCreateAPIView.as_view(), name='new-product'),
+    path('list/', views.ProductListAPIView.as_view(), name='product-list'),
+    path('<str:pk>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('<str:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-update'),
+    path('<str:pk>/destroy/', views.ProductDestroyAPIView.as_view(), name='product-destroy'),
 ]
